9b948004ebfdd98999b285a84bf38383f4b26b2f
Henry García <henrygarciaospina@gmail.com>
2025-06-13 04:07:20 -0500

    Correct tailwind configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
       Welcome <FaBeer />
-      <h1 className="text-4xl text-red-600 font-bold">¡Tailwind está funcionando!</h1>
     </div>
   );
 }
