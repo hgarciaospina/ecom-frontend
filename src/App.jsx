883ced2266ed18f58a9c30fef28492b387e55395
Henry García <henrygarciaospina@gmail.com>
2025-06-13 05:40:15 -0500

    Getting started with product component to set layout.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './index.css';
 import { useState } from 'react';
-import { FaBeer } from 'react-icons/fa';
+import Products from './components/products';
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
-      Welcome <FaBeer />
-    </div>
+    <> 
+      <Products />
+    </>
   );
 }
 
