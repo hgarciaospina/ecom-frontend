caabce523248bd765a37770feea6898a18992318
Henry García <henrygarciaospina@gmail.com>
2025-06-16 16:48:05 -0500

    Managing loaders and error scenarios

--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { errorReducer } from "./errorReducer";
 import { productReducer } from "./ProductReducer";
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    errors: errorReducer,
   },
   preloadedState: {},
 });
